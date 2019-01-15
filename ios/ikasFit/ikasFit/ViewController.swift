b0db1294eb024956b023c47bca4a4c78086d17d9
Carlos <carlos95egibide@gmail.com>
2019-01-15 16:13:03 +0100

    Proyecto Xcode creado

---
+++ ios/ikasFit/ikasFit/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ikasFit
+//
+//  Created by Carlos Hernández on 15/01/2019.
+//  Copyright © 2019 Carlos Hernández. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
