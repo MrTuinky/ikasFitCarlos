ad605e54757789e4e5b407635417ce046b8897cb
Carlos <carlos95egibide@gmail.com>
2019-01-15 19:49:59 +0100

    Dar permisos para acceder a los pasos.
    Aparece una pantalla nada más inicializar la aplicación que te da la opción de dar permiso a que la aplicación lea y escriba los pasos que realizas.

--- ios/ikasFit/ikasFit/ViewController.swift
+++ ios/ikasFit/ikasFit/ViewController.swift
@@ -7,14 +7,56 @@
 //
 
 import UIKit
+import HealthKit
+import HealthKitUI
+
+let healthStore = HKHealthStore()
+
+
+
 
 class ViewController: UIViewController {
+    
+    
+    
 
+    @IBOutlet weak var labelPasos: UILabel!
+    
+    @IBAction func botonActualizar(_ sender: Any) {
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        autorizar()
+        
+        
+        
     }
+    
+    // Para dar permisos
+    // Idea para el codigo: https://developer.apple.com/documentation/healthkit/setting_up_healthkit
+    
+    func autorizar(){
+        
+        let allTypes = Set([HKObjectType.quantityType(forIdentifier: .stepCount)!])
+        
+        healthStore.requestAuthorization(toShare: allTypes, read: allTypes) { (success, error) in
+            if !success {
+                print("errooooooooooooooooooor")
+            }
+        }
 
+        
+    }
+    
+    
+    
+    
+    
+    
 
 }
+
 
