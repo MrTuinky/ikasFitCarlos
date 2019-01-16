9ac4fd32f2f996cc34aa345c83541067cfc0beca
Carlos <carlos95egibide@gmail.com>
2019-01-16 14:11:40 +0100

    Acceso a los pasos y mostrarlos por pantalla, etc
    Acceso a los pasos y mostrarlos por pantalla, nuevo StoryBoard con botón para aceder a los Ajustes del iPhone

--- ios/ikasFit/ikasFit/ViewController.swift
+++ ios/ikasFit/ikasFit/ViewController.swift
@@ -20,10 +20,6 @@ class ViewController: UIViewController {
     
     
 
-    @IBOutlet weak var labelPasos: UILabel!
-    
-    @IBAction func botonActualizar(_ sender: Any) {
-    }
     
     
     override func viewDidLoad() {
@@ -32,8 +28,40 @@ class ViewController: UIViewController {
         autorizar()
         
         
+    }
+    
+    
+    //Botón para ir a Salud y activar que deje leer los pasos
+    //Fuente https://stackoverflow.com/questions/28152526/how-do-i-open-phone-settings-when-a-button-is-clicked/52103305
+    
+    @IBAction func botonAutorizar(_ sender: Any) {
+        //UIApplication.shared.open(URL(string: UIApplication.openSettingsURLString)!)
+        
+        
+        let alertController = UIAlertController (title: "Autorizar", message: "¿Quieres que la aplicación abra los Ajustes del iPhone?", preferredStyle: .alert)
+            
+            let settingsAction = UIAlertAction(title: "Ajustes", style: .default) { (_) -> Void in
+                
+                guard let settingsUrl = URL(string: UIApplication.openSettingsURLString) else {
+                    return
+                }
+                
+                if UIApplication.shared.canOpenURL(settingsUrl) {
+                    UIApplication.shared.open(settingsUrl, completionHandler: { (success) in
+                        print("Ajstes abiertos: \(success)") // Prints true
+                    })
+                }
+            }
+            alertController.addAction(settingsAction)
+            let cancelAction = UIAlertAction(title: "Cancelar", style: .cancel, handler: nil)
+            alertController.addAction(cancelAction)
+            
+            present(alertController, animated: true, completion: nil)
+        
         
     }
+    
+    
     
     // Para dar permisos
     // Idea para el codigo: https://developer.apple.com/documentation/healthkit/setting_up_healthkit
@@ -43,12 +71,68 @@ class ViewController: UIViewController {
         let allTypes = Set([HKObjectType.quantityType(forIdentifier: .stepCount)!])
         
         healthStore.requestAuthorization(toShare: allTypes, read: allTypes) { (success, error) in
-            if !success {
-                print("errooooooooooooooooooor")
+            if success {
+                print("Bien leído")
             }
         }
+        
+        
 
         
+    }
+    
+    // Para que recoja los pasos hechos en todo el día
+    // Fuente: https://stackoverflow.com/questions/36559581/healthkit-swift-getting-todays-steps
+    
+    func pasosDeHoy(completion: @escaping (Double) -> Void) {
+        
+        let stepsQuantityType = HKQuantityType.quantityType(forIdentifier: .stepCount)!
+        
+        let now = Date()
+        let startOfDay = Calendar.current.startOfDay(for: now)
+        let predicate = HKQuery.predicateForSamples(withStart: startOfDay, end: now, options: .strictStartDate)
+        
+        let query = HKStatisticsQuery(quantityType: stepsQuantityType, quantitySamplePredicate: predicate, options: .cumulativeSum) { (_, result, error) in
+            var resultCount = 0.0
+            guard let result = result else {
+                
+                //En caso de que al iniciar la aplicación el usuario le de a no permitir...
+                // TODO implementar botón en los ajustes "Autorizar HealthKit"
+                
+                print("Error al obtener los pasos de healthkit (posiblemente porque no le hayan dado acceso)")
+                completion(resultCount)
+                return
+            }
+            if let sum = result.sumQuantity() {
+                resultCount = sum.doubleValue(for: HKUnit.count())
+            }
+            
+            DispatchQueue.main.async {
+                completion(resultCount)
+            }
+        }
+        healthStore.execute(query)
+    }
+    
+    
+    
+    @IBOutlet weak var labelPasos: UILabel!
+    
+    @IBAction func botonActualizar(_ sender: Any) {
+        
+        
+        
+        pasosDeHoy { (result) in
+            print("\(result)")
+            DispatchQueue.main.async {
+                self.labelPasos.text = "\(result)"
+            }
+        
+        
+        
+    }
+    
+   
     }
     
     
@@ -56,7 +140,9 @@ class ViewController: UIViewController {
     
     
     
-
 }
+    
+
+
 
 
