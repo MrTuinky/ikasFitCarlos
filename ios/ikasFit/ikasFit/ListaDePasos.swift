68c2a4561adb837601f18b448118cb5c29467ee1
Carlos <carlos95egibide@gmail.com>
2019-01-17 11:59:16 +0100

    Creado un TableView
    He creado un TableView para mostrar en forma de table todos los registros que se han subido a Firestore.
    
    Muestra todo, falta ordenarlos de mayor a menos y que no haya duplicados y ordenarlos por usuarios anónimos.

---
+++ ios/ikasFit/ikasFit/ListaDePasos.swift
@@ -0,0 +1,13 @@
+//
+//  ListaDePasos.swift
+//  ikasFit
+//
+//  Created by Carlos Hernández on 17/01/2019.
+//  Copyright © 2019 Carlos Hernández. All rights reserved.
+//
+
+import Foundation
+
+struct ListaDePasos {
+    var pasos: String
+}
--- ios/ikasFit/ikasFit/ViewController.swift
+++ ios/ikasFit/ikasFit/ViewController.swift
@@ -39,8 +39,6 @@ class ViewController: UIViewController {
         
         
         
-        
-        
     }
     
     
